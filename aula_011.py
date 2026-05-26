38471560dedd216496d2a78c1dd24ac990eedccf
victorvicdev <vic.desenhos@gmail.com>
2026-05-26 02:35:22 -0300

    Aulas - 26-05-26

---
+++ aula_011.py
@@ -0,0 +1,13 @@
+#print('\033[4;33mOlá Mundo!\033[m')
+nome = 'Guanabara'
+#print('Olá. Muito prazer em te conhecer, {}{}{}!!!'.format('\033[4;34m', nome, '\033[m'))
+i = '\033[4;34m'
+f = '\033[m'
+print('Olá. Muito prazer em te conhecer, {}{}{}!!!'.format(i, nome, f))
+
+#cores = {'limpa':'\033[m',
+       # 'azul':'\033[34m',
+        #'amarelo':'\033[33m',
+        #'pretoebranco':'\033[7;40m'}
+#print('Olá. Muito prazer em te conhecer, {}{}{}!!'.format(cores['pretoebranco'], nome, cores['limpa']))
+
